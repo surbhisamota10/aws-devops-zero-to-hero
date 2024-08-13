58194dcdf99086195c84d497d0865382cdf70741
surbhisamota10 <95540023+surbhisamota10@users.noreply.github.com>
2024-08-14 00:51:35 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull samotasurbhi/simple-python-flask-app
 
-# Run the Docker image as a container
+# Run the Docker image as a  container
 docker run -d -p 7000:7000 samotasurbhi/simple-python-flask-app
